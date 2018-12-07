ebf3eda24bbfdadd390bed603166de9c5fc1d2fc
cybon <kimon5@hotmail.dk>
2018-12-07 13:31:42 +0100

    it works

--- Script.py
+++ Script.py
@@ -6,7 +6,9 @@ import json
 
 from sense_hat import SenseHat
 
-recPort = 6000
+globalRecPort = 6000
+sensorRecPort = 0
+
 shouldWater = 0
 
 BROADCAST_TO_PORT = 6666
@@ -16,7 +18,10 @@ minutes = 10
 sense = SenseHat()
 sense.clear()
 
-def struct
+
+def water_now():
+    print("Watering!")
+
 
 def get_mac(interface='wlan0'):
     try:
@@ -26,29 +31,42 @@ def get_mac(interface='wlan0'):
     return mac[0:17]
 
 
-# UDP Proxy Broadcast
-s = socket(AF_INET, SOCK_DGRAM)
-s.bind(('', recPort))     # (ip, port)
-# no explicit bind: will bind to default IP + random port
-s.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
+if sensorRecPort == 0:
+    s1 = socket(AF_INET, SOCK_DGRAM)
+    s1.bind(('', globalRecPort))
+    s1.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
+    s1.sendto(bytes(str(get_mac()), "UTF-8"), ('<broadcast>', BROADCAST_TO_PORT))
+    time.sleep(5)
+    jsonObj1 = str(s1.recvfrom(1024))
+    splitObj = jsonObj1.split("'")
+    portObj = json.loads(splitObj[1])
+    if portObj["macAddress"] == str(get_mac()):
+        sensorRecPort = int(portObj["port"])
+        print("New port set!")
 
-while True:
-    if recPort == 6000:
-        s.sendto(bytes(str(get_mac()), "UTF-8"), ('<broadcast>', BROADCAST_TO_PORT))
-        time.sleep(30)
-        jsonList = str(s.recvfrom(1024))
-        print(jsonList)
-    else:
-        humidity = sense.get_humidity()
 
-        jsonObj = {
-            "fk_macaddress": str(get_mac()),
-            "humidity": str(humidity),
-            "date": str(datetime.now())
-        }
-        data = json.dumps(jsonObj)
-        s.sendto(bytes(data, "UTF-8"), ('<broadcast>', BROADCAST_TO_PORT))
-        time.sleep(30)
-        shouldWater = s.recvfrom(1024)
-        print(shouldWater)
-        time.sleep(60 * minutes)
+if sensorRecPort != 0:
+    s2 = socket(AF_INET, SOCK_DGRAM)
+    s2.bind(('', sensorRecPort))
+    s2.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
+    while True:
+            print("Check if you are too dry and should water!")
+            humidity = sense.get_humidity()
+
+            jsonObj2 = {
+                "fk_macaddress": str(get_mac()),
+                "humidity": str(humidity),
+                "date": str(datetime.now())
+            }
+
+            data = json.dumps(jsonObj2)
+            s2.sendto(bytes(data, "UTF-8"), ('<broadcast>', BROADCAST_TO_PORT))
+            time.sleep(5)
+            jsonObjStr = str(s2.recvfrom(1024))
+            splitObj2 = jsonObjStr.split("'")
+            shouldWater = int(splitObj2[1])
+            if shouldWater == 1:
+                water_now()
+            time.sleep(60 * minutes)
+
+
